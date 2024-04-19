a9d6a9f6aa39b83ff657498b1ac9c561348726cc
Marceline-sama <oninoniisan@gmail.com>
2024-04-19 07:19:03 -0300

    metodos de soma em subtração criados

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
+import operacoes
+
 print("calculadora")
-p_numero = int(input("digite o primeiro numero para calcular: "))
+
 
 op = int(input("""digite 1 para realizar uma soma de 2 numeros\n
            digite 2 para realizar uma subtração de 2 numeros\n
            digite 3 para realizar uma multiplicação de 2 numeros\n
            digite 4 para realizar a divisão de 2 numeros: \n"""))
 if (op == 1):
+    num1 = int(input("digite o primeiro numero para calcular: "))
+    num2 = int(input("digite o primeiro numero para calcular: "))
+    operacao = operacoes()
+    resultado = operacoes.soma(num1, num2)
     pass#metodo da soma
 if (op == 2):
+    num1 = int(input("digite o primeiro numero para calcular: "))
+    num2 = int(input("digite o primeiro numero para calcular: "))
+    operacao = operacoes()
+    resultado = operacoes.sub(num1, num2)
     pass#metodo da subtração
 if (op == 3):
     pass#metodo da multiplicação
