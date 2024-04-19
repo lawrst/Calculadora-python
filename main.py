db583d17afced6f4ec1918b2eba47c118345b71c
Marceline-sama <oninoniisan@gmail.com>
2024-04-19 07:09:27 -0300

    inicio da calculadora

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 print("calculadora")
-p_numero = input("digite o primeiro numero para calcular: ")
+p_numero = int(input("digite o primeiro numero para calcular: "))
 
-operacao = input("Escolha qual a operação deseja fazer ")
-
-print("banana")
+op = int(input("""digite 1 para realizar uma soma de 2 numeros\n
+           digite 2 para realizar uma subtração de 2 numeros\n
+           digite 3 para realizar uma multiplicação de 2 numeros\n
+           digite 4 para realizar a divisão de 2 numeros: \n"""))
+if (op == 1):
+    pass#metodo da soma
+if (op == 2):
+    pass#metodo da subtração
+if (op == 3):
+    pass#metodo da multiplicação
+if (op == 4):
+    pass#metodo da divisão
 
