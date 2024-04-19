16f9a7ac11e5984b542892eda172e8fc09c4e1d0
Lawrence Lopes <81780029+lawrst@users.noreply.github.com>
2024-04-19 16:10:11 -0300

    Update operacoes.py
    Operação de divisão

--- operacoes.py
+++ operacoes.py
@@ -4,7 +4,9 @@ class operacoes:
     
     def sub(self, num1, num2):
         return num1 - num2
+        
     def multi(self, num1, num2):
         pass
+        
     def div(self, num1, num2):
-        pass
+        return num1 / num2
